b5934e36c09d504826737f9ae88d84cbb5247a48
malcolmreis02 <malcolm.reis02@gmail.com>
2022-02-15 10:46:16 -0300

    .

--- src/components/invite/invite.js
+++ src/components/invite/invite.js
@@ -3,18 +3,18 @@ import { useState, useEffect } from 'react'
 
 const Invite = () => {
 
-    const url = 'http://localhost:5000/message'
+    const url = 'http://localhost:5000/women'
     const [author, setAuthor] = useState('')
     const [content, setContent] = useState('')
     
-    const sendMessage = () => {
+    const sendData = () => {
         if (author.length <= 0 || content.length<=0) {
             alert('You did not fill the forms right. Please, input all the informations')
         }
 
         const bodyForm = {
-            email: author,
-            message: content,
+            name: author,
+            cellphone: content,
         }
 
         fetch(url, {
@@ -25,13 +25,13 @@ const Invite = () => {
             body: JSON.stringify(bodyForm)
         })
 
-        alert(`OK, you can wait my return by the email: ${author}`)
+        alert(`OK, ${author}, I will call you! `)
     }
 
    useEffect(async() => {
-       const response = await fetch(url)
+        const response = await fetch(url)
         const data = await response.json()
-       console.log(data)
+        console.log(data)
    }, [])
     
 
@@ -46,18 +46,18 @@ const Invite = () => {
                         <h2>Let's go out for a dinner</h2>
                     </div>
                     <div className='form'>
-                        <h5>What is your email for I contact you?</h5>
+                        <h5>First of all, what is your name?</h5>
                             <div className="form-floating mb-3">
                                 <input type="text" value={author} onChange={(event)=>(setAuthor(event.target.value))} className="form-control opacity-75" id="floatingInput" placeholder="name@example.com"/>
-                                <label for="floatingInput">Email</label>
+                                <label htmlFor="floatingInput">Name</label>
                             </div>
-                        <h5>Do you want give me a message?</h5>
+                        <h5>Can I call you by WhatsApp?</h5>
                             <div className="form-floating">
                                 <input type="text" value={content} onChange={(event)=>(setContent(event.target.value))} className="form-control opacity-75" id="floatingPassword" placeholder="Password"/>
-                                <label for="floatingPassword">Message</label>
+                                <label htmlFor="floatingPassword">(00)12345-6789</label>
                             </div>
                             <div className='d-grid gap-2 col-6 mx-auto'>
-                                <button type="button" onClick={sendMessage} className="submit btn btn-outline-light">Call me, please!</button>
+                                <button type="button" onClick={sendData} className="submit btn btn-outline-light">Call me, please!</button>
                             </div>
                     </div>
                 </div>
